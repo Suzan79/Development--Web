24c8938a0d90d8eb010bedfeaa09eae6b89198bc
Suzan79 <Suzan79@users.noreply.github.com>
2020-01-23 23:45:51 +0100

    uitpakken

---
+++ Lesson2/lesson2_console_app/lesson2_console_app/Models/Model.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+
+namespace lesson2_console_app.Model
+{
+    public class MovieContext : DbContext
+    {
+        //this is actual entity object linked to the movies in our DB
+        public DbSet<Movie> Movies { get; set; }
+        //this is actual entity object linked to the actors in our DB
+        public DbSet<Actor> Actors { get; set; }
+
+        //this method is run automatically by EF the first time we run the application
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //here we define the name of our database
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=MovieDB;Pooling=true;");
+        }
+    }
+
+    //this is the typed representation of a movie in our project
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Release { get; set; }
+        public List<Actor> Actors { get; set; }
+    }
+
+    //this is the typed representation of an actor in our project
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+        public string Gender { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        
+    }
+}
---
+++ Lesson2/lesson2_console_app/lesson2_console_app/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using lesson2_console_app.Model;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace lesson2_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeDb();
+            Console.WriteLine("Done.");
+        }
+        static void InitializeDb()
+        {
+
+            using (var db = new MovieContext())
+            {
+                if (db.Movies.Count() == 0 && db.Actors.Count() == 0)
+                {
+                    Movie m1 = new Movie
+                    {
+                        Title = "No country for old men",
+                        Release = new DateTime(2008, 2, 14),
+                        Actors = new List<Actor> {
+                        new Actor{
+                            Name = "Tommy Lee",
+                            Birth = new DateTime(1962,10,3),
+                            Gender = "Male"},
+                        new Actor{
+                            Name = "Xavier Berdem",
+                            Birth = new DateTime(1969,03,1),
+                            Gender = "Male"}
+                            }
+                    };
+                    Movie m2 = new Movie
+                    {
+                        Title = "Star Wars",
+                        Release = new DateTime(2018, 11, 11),
+                        Actors = new List<Actor> {
+                        new Actor{
+                            Name = "Harrison Ford",
+                            Birth = new DateTime(1942,07,13),
+                            Gender = "Male"}
+                        }
+                    };
+                    Movie m3 = new Movie
+                    {
+                        Title = "Titanic",
+                        Release = new DateTime(1998, 1, 15),
+                        Actors = new List<Actor> {
+                        new Actor{
+                            Name = "Leonardo Di Caprio",
+                            Birth = new DateTime(1974,11,11),
+                            Gender = "Male"},
+                        new Actor{
+                            Name = "Kate Winslet",
+                            Birth = new DateTime(1975,10,5),
+                            Gender = "Female"}
+                    }
+                    };
+
+                    db.Movies.Add(m1);
+                    db.Movies.Add(m2);
+                    db.Movies.Add(m3);
+
+                    db.SaveChanges();
+                }
+
+
+            }
+
+        }
+    }
+}
